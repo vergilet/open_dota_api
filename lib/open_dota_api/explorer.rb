87824dae4a49fd8c13cad33b248b3e492bdf4bae
YaroslavO <yaroslavo@softwareplanetgroup.com>
2017-08-27 17:28:28 +0300

    remove white spaces

--- lib/open_dota_api/explorer.rb
+++ lib/open_dota_api/explorer.rb
@@ -7,7 +7,7 @@ module OpenDotaApi
     def self.instantiate(_ = nil)
       raise NotImplementedError
     end
-    
+
     def self.query_params(league_id)
       {
         sql: "SELECT matches.match_id FROM matches WHERE matches.leagueid = #{league_id} ORDER BY matches.match_id LIMIT 20000"
